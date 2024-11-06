6cafffd70593248327d59c45de46dc224a8adf1f
Nartallax <nartallax@gmail.com>
2024-11-06 08:34:22 +0100

    1.0.15: restartIfChanged

--- src/build_utils/run_js.ts
+++ src/build_utils/run_js.ts
@@ -1,6 +1,8 @@
 import * as Process from "process"
 import * as ChildProcess from "child_process"
 import {runShell, ShellRunOptions, startProcess} from "shell"
+import * as Crypto from "crypto"
+import {promises as Fs} from "fs"
 
 export type RunJsOptions = Omit<ShellRunOptions, "executable"> & {
 	jsFile: string
@@ -16,6 +18,7 @@ export const runJs = async(options: RunJsOptions) => {
 
 export type JsProcess = {
 	restart: () => Promise<void>
+	restartIfChanged: () => Promise<void>
 	getProcess: () => ChildProcess.ChildProcess | null
 }
 
@@ -28,7 +31,14 @@ export type StartJsProcessOptions = RunJsOptions & {
 export const startJsProcess = async(options: StartJsProcessOptions): Promise<JsProcess> => {
 	let process: ChildProcess.ChildProcess | null = null
 
-	const startIt = () => new Promise<void>((ok, bad) => {
+	const getHash = async() => Crypto.createHash("sha256").update(await Fs.readFile(options.jsFile)).digest("hex")
+
+	let lastRunCodeHash = ""
+	const updateHash = async(hash?: string) => {
+		lastRunCodeHash = hash ?? await getHash()
+	}
+
+	const startIt = (hash?: string) => new Promise<void>((ok, bad) => {
 		process = startProcess({
 			...options,
 			executable: options.nodeJsPath ?? Process.argv[0]!,
@@ -39,26 +49,38 @@ export const startJsProcess = async(options: StartJsProcessOptions): Promise<JsP
 		process.once("exit", () => {
 			process = null
 		})
+
+		void updateHash(hash)
 	})
 
 	await startIt()
 
+	const restart = (hash?: string) => {
+		if(process){
+			const proc = process
+			return new Promise<void>(ok => {
+				proc.once("exit", async() => {
+					await startIt(hash)
+					ok()
+				})
+				proc.kill(options.restartSignal ?? "SIGINT")
+			})
+
+		} else {
+			return startIt(hash)
+		}
+	}
+
+	const restartIfChanged = async() => {
+		const newHash = await getHash()
+		if(lastRunCodeHash !== newHash){
+			await restart(newHash)
+		}
+	}
+
 	return {
 		getProcess: () => process,
-		restart: () => {
-			if(process){
-				const proc = process
-				return new Promise<void>(ok => {
-					proc.once("exit", async() => {
-						await startIt()
-						ok()
-					})
-					proc.kill(options.restartSignal ?? "SIGINT")
-				})
-
-			} else {
-				return startIt()
-			}
-		}
+		restart,
+		restartIfChanged
 	}
 }
