91c32b30423629f8878ce3d43353ed6cafae6e1b
Nartallax <nartallax@gmail.com>
2025-01-01 19:01:15 +0100

    1.2.2: Fix exported types not being exported in .d.ts

--- src/build_utils/dts.ts
+++ src/build_utils/dts.ts
@@ -1,4 +1,4 @@
-import {promises as Fs} from "fs"
+import {npx} from "build_utils/npx"
 
 export type GenerateDtsOptions = {
 	inputFile: string
@@ -9,20 +9,23 @@ export type GenerateDtsOptions = {
 }
 
 export const generateDts = async(options: GenerateDtsOptions) => {
-	const result = (await import("dts-bundle-generator")).generateDtsBundle([{
-		filePath: options.inputFile,
-		output: {
-			noBanner: true,
-			exportReferencedTypes: false
+	// it's tempting to `(await import("dts-bundle-generator")).generateDtsBundle(...)`
+	// but CLI args have different effect, compared to calling API directly
+	// for example, `--export-referenced-types false` in CLI will only prevent exporting referenced-but-not-exported types
+	// (as it should be)
+	// but `exportReferencedTypes: false` in API will prevent export of all types, even explicitly exported ones
+	await npx(
+		["dts-bundle-generator",
+			options.inputFile,
+			...(options.banner ? [] : ["--no-banner"]),
+			"--export-referenced-types",
+			options.exportReferencedTypes ? "true" : "false",
+			"-o",
+			options.outputFile,
+			"--project",
+			options.tsconfigPath],
+		{
+			exitOnError: true
 		}
-	}], {
-		preferredConfigPath: options.tsconfigPath
-	})
-
-	if(result.length !== 1){
-		throw new Error(`Unexpected output from dts-bundle-generator: ${result.length} entries`)
-	}
-
-	const dtsCode = result[0]!
-	await Fs.writeFile(options.outputFile, dtsCode, "utf-8")
+	)
 }
